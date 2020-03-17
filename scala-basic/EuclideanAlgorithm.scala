0ef0c1bba57975a73a7f008e90b01df5892d96a5
solareenlo <solareenlo@gmail.com>
2020-03-17 20:40:29 +0900

    add ユークリッドの互除法

---
+++ scala-basic/EuclideanAlgorithm.scala
@@ -0,0 +1,11 @@
+object EuclideanAlgorithm extends App {
+  def greatestCommonDivisor(a: Int, b: Int): Int = {
+    if (a == 0) {
+      b
+    } else if (b == 0) {
+      a
+    } else {
+      greatestCommonDivisor(b, a % b)
+    }
+  }
+}
