20480b8288f3d667ce318895a958e6cd7646ac82
solareenlo <solareenlo@gmail.com>
2020-03-17 13:51:12 +0900

    add factorial

---
+++ scala-basic/Factorial.scala
@@ -0,0 +1,6 @@
+import scala.math.BigInt
+
+object Factorial extends App {
+  def factorial(i: BigInt, acc: BigInt): BigInt = if (i == 0) acc else factorial (i - 1, i * acc)
+  println(factorial(1000, 1))
+}
